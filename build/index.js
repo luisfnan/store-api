4d06951decc233a63fc21a772682c4b126b0ab61
luisfnan <fernando-9813@hotmail.com>
2023-08-16 22:22:51 -0400

    port

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const products_routes_1 = __importDefault(require("./src/products.routes"));
@@ -21,7 +22,7 @@ app.use('/api', order_routes_1.default);
 app.get('/api', (req, res) => {
     res.send('Bienvenidos!');
 });
-const PORT = 3000;
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });
